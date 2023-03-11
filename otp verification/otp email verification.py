b4503e70e437754a7c7f5968fe205770e3aa4872
Aunmohammed110 <123323484+Aunmohammed110@users.noreply.github.com>
2023-03-11 19:15:41 +0530

    otp email verification

---
+++ otp verification/otp email verification.py
@@ -0,0 +1,16 @@
+import random
+import smtplib
+server=smtplib.SMTP('smtp.gmail.com',587)
+#adding TLS security 
+server.starttls()
+#get your app password of gmail
+password='edifvdkkisvuyucl'
+server.login("aunmohammed110@gmail.com",password)
+#generate OTP using random.randint() function
+otp=''.join([str(random.randint(0,9)) for i in range(6)])
+msg='Hello, Your OTP is '+str(otp)
+sender='aunmohammed110@gmail.com'  #write email id of sender
+receiver='sirtesla52@gmail.com' #write email of receiver
+
+server.sendmail(sender,receiver,msg)
+server.quit()
